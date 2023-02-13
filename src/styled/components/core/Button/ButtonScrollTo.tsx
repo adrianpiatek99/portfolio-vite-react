e19a85a7d29fe7617fd3d9b08ff065342b60a54f
Adrian Piątek <adr654231@gmail.com>
2023-02-13 11:22:07 +0100

    feat: Add buttons (#2)
    * Add generalButtonStyles
    
    * Add ripple effect util
    
    * Add button component
    
    * Add buttonScrollTo component

---
+++ src/styled/components/core/Button/ButtonScrollTo.tsx
@@ -0,0 +1,50 @@
+import type { ComponentPropsWithRef, MouseEvent, Ref } from "react";
+import { forwardRef } from "react";
+
+import styled, { useTheme } from "styled-components";
+import { handleCreateRippleEffect } from "utils/rippleUtils";
+
+import type { ButtonElementProps, ButtonTextAlign } from "./buttonStyle";
+import { generalButtonStyles } from "./buttonStyle";
+
+interface ButtonScrollToProps extends ComponentPropsWithRef<"button"> {
+  elementId: string;
+  fullWidth?: boolean;
+  textAlign?: ButtonTextAlign;
+}
+
+export const ButtonScrollTo = forwardRef(
+  (
+    { children, elementId, fullWidth = false, textAlign = "center", ...props }: ButtonScrollToProps,
+    ref: Ref<HTMLButtonElement>
+  ) => {
+    const { primary } = useTheme();
+
+    const handleClick = (e: MouseEvent<HTMLButtonElement>) => {
+      const element = document.getElementById(elementId);
+
+      if (element) {
+        element.scrollIntoView({ behavior: "smooth" });
+      }
+
+      handleCreateRippleEffect(e, primary);
+      props.onClick?.(e);
+    };
+
+    return (
+      <ButtonElement
+        $fullWidth={fullWidth}
+        textAlign={textAlign}
+        {...props}
+        onClick={handleClick}
+        ref={ref}
+      >
+        <span>{children}</span>
+      </ButtonElement>
+    );
+  }
+);
+
+const ButtonElement = styled.button<ButtonElementProps>`
+  ${generalButtonStyles};
+`;
