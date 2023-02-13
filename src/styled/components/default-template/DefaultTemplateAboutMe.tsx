488a2b5c7640cba21a460e528786f64358c63a0a
Adrian Piątek <adr654231@gmail.com>
2023-02-13 12:03:55 +0100

    Add about me section

---
+++ src/styled/components/default-template/DefaultTemplateAboutMe.tsx
@@ -0,0 +1,87 @@
+import { IconButton, Typography } from "components/core";
+import GithubIcon from "icons/GithubIcon";
+import styled, { useTheme } from "styled-components";
+
+export const DefaultTemplateAboutMe = () => {
+  const { primary } = useTheme();
+
+  return (
+    <AboutMeSection id="about-me">
+      <Content>
+        <ContentLeft>
+          <Typography heading tag="h2" weight={600} size="xxl" mobileSize="l">
+            Adrian Piątek
+          </Typography>
+        </ContentLeft>
+        <ContentRight>
+          <Typography
+            tag="p"
+            weight={600}
+            color="secondary"
+            size="xxl"
+            mobileSize="xl"
+            customColor={primary}
+          >
+            Junior React Developer
+          </Typography>
+          <Typography tag="p" color="secondary" size="xl" mobileSize="l">
+            Lorem ipsum dolor sit amet consectetur adipisicing elit. Culpa veritatis cumque natus
+            minima? Hic similique ipsam dicta molestias quibusdam rem vero nisi facere nulla, eaque
+            quidem blanditiis laborum deleniti mollitia!
+          </Typography>
+          <IconsGroup>
+            <IconButton title="GitHub" size="large">
+              <GithubIcon />
+            </IconButton>
+          </IconsGroup>
+        </ContentRight>
+      </Content>
+    </AboutMeSection>
+  );
+};
+
+const AboutMeSection = styled.section`
+  display: flex;
+  flex-direction: column;
+  min-height: 65vh;
+`;
+
+const Content = styled.div`
+  display: flex;
+  flex-direction: column;
+  gap: 30px;
+  width: 100%;
+  margin: 84px auto;
+
+  @media ${({ theme }) => theme.breakpoints.sm} {
+    margin: 124px auto;
+  }
+
+  @media ${({ theme }) => theme.breakpoints.lg} {
+    flex-direction: row;
+  }
+`;
+
+const ContentLeft = styled.div`
+  display: flex;
+  flex-direction: column;
+  width: 100%;
+  height: 100%;
+
+  @media ${({ theme }) => theme.breakpoints.lg} {
+    position: sticky;
+    top: 70px;
+  }
+`;
+
+const ContentRight = styled.div`
+  display: flex;
+  flex-direction: column;
+  width: 100%;
+  gap: 30px;
+`;
+
+const IconsGroup = styled.div`
+  display: flex;
+  gap: 12px;
+`;
