8b6b81401cb071f0f34d98bac066a8921f3957b6
Adrian Piątek <adr654231@gmail.com>
2023-02-13 11:21:22 +0100

    Add button component

---
+++ src/styled/components/core/Button/Button.tsx
@@ -0,0 +1,43 @@
+import type { ComponentPropsWithRef, MouseEvent, Ref } from "react";
+import { forwardRef } from "react";
+
+import styled from "styled-components";
+import { handleCreateRippleEffect } from "utils/rippleUtils";
+
+import type { ButtonElementProps, ButtonTextAlign } from "./buttonStyle";
+import { generalButtonStyles } from "./buttonStyle";
+
+interface ButtonProps extends ComponentPropsWithRef<"button"> {
+  fullWidth?: boolean;
+  textAlign?: ButtonTextAlign;
+}
+
+export const Button = forwardRef(
+  (
+    { children, fullWidth = false, textAlign = "center", ...props }: ButtonProps,
+    ref: Ref<HTMLButtonElement>
+  ) => {
+    const handleClick = (e: MouseEvent<HTMLButtonElement>) => {
+      handleCreateRippleEffect(e);
+
+      props.onClick?.(e);
+    };
+
+    return (
+      <ButtonElement
+        $fullWidth={fullWidth}
+        textAlign={textAlign}
+        {...props}
+        onClick={handleClick}
+        ref={ref}
+      >
+        {children}
+      </ButtonElement>
+    );
+  }
+);
+
+const ButtonElement = styled.button<ButtonElementProps>`
+  ${generalButtonStyles};
+  background-color: ${({ theme }) => theme.primary};
+`;
